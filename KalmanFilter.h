08305d397212cf3a88dc5aa5e3606d996e4c4012
Bigatos39 <162310278+Bigatos39@users.noreply.github.com>
2024-03-05 18:47:49 +0700

    Add files via upload

---
+++ KalmanFilter.h
@@ -0,0 +1,22 @@
+#ifndef KALMANFILTER_H
+#define KALMANFILTER_H
+
+#include "Track.h"
+
+class KalmanFilter
+{
+	public:
+		KalmanFilter();
+
+		void Fit(Track &track);
+	private:
+		void Initilize(Track &track) const;
+		void Predict(Track &track, float zNew) const;
+		void Noise(Track &track) const;
+		void Update(Track &track, float xNew) const;
+
+		float fScatter;
+		float fSigma;
+};
+
+#endif
